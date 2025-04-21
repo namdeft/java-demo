b5996a48520fc5a69783e962aca14bcafb61ab88
namdeft <dinhnam2306@gmail.com>
2025-04-21 17:03:07 +0700

    add base response

---
+++ src/main/java/com/example/demo/dto/response/BaseResponse.java
@@ -0,0 +1,33 @@
+package com.example.demo.dto.response;
+
+import com.example.demo.constant.CommonMsg;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseResponse<T> {
+  private boolean success;
+  private long code;
+  private String message;
+  private T data;
+
+  public BaseResponse(long code, String message, T data) {
+    this.success = code == 0;
+    this.code = code;
+    this.message = message;
+    this.data = data;
+  }
+
+  public static <T> BaseResponse<T> success(long code, String message, T data) {
+    return new BaseResponse<>(code, message, data);
+  }
+
+  public static <T> BaseResponse<T> success(CommonMsg msg, T data) {
+    return new BaseResponse<>(msg.getCode(), msg.getMessage(), data);
+  }
+
+  public static <T> BaseResponse<T> error(CommonMsg msg) {
+    return new BaseResponse<>(msg.getCode(), msg.getMessage(), null);
+  }
+}
--- src/main/java/com/example/demo/logger/StartupLogger.java
+++ src/main/java/com/example/demo/logger/StartupLogger.java
@@ -1,6 +1,7 @@
 package com.example.demo.logger;
 
 import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,25 +11,23 @@ import org.slf4j.LoggerFactory;
 @Component
 public class StartupLogger {
 
+  @Value("${host}")
+  private String host;
+
+  @Value("${port}")
+  private String port;
+
   private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-
-  private final Environment env;
-
-  public StartupLogger(Environment env) {
-    this.env = env;
-  }
 
   @EventListener(ApplicationReadyEvent.class)
   public void logSwaggerUrl() {
-    String host = env.getProperty("server.host");
-    String port = System.getProperty("server.port");
     if (host == null || host.isEmpty()) {
       host = "localhost";
     }
     if (port == null || port.isEmpty()) {
       port = "8080";
     }
-    String swaggerUrl = "http://" + host + ":" + port + "/api/v1/swagger-ui/index.html";
+    String swaggerUrl = "http://" + host + port + "/api/v1/swagger-ui/index.html";
     logger.info("Swagger UI is available at: {}", swaggerUrl);
   }
 }
